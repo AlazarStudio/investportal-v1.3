f8c581baebf265b42f15d6982f29e9464150ba5e
alimdzhatdoev <alimdzhatdoev1@gmail.com>
2024-05-07 15:22:02 +0300

    Update

--- src/Components/Blocks/Footer/Footer.jsx
+++ src/Components/Blocks/Footer/Footer.jsx
@@ -1,12 +1,76 @@
 import React from "react";
 import classes from './Footer.module.css';
+import CenterBlock from "../../Standart/CenterBlock/CenterBlock";
+import WidthBlock from "../../Standart/WidthBlock/WidthBlock";
 
 function Footer({ children, ...props }) {
-    return ( 
+    return (
         <>
-            Footer
+            <div className={classes.footer}>
+                <CenterBlock>
+                    <WidthBlock>
+                        <div className={classes.footer_logoLine}>
+                            <div className={classes.footer_logoLine__left}>
+                                <div className={classes.footer_logoLine__left___img}>
+                                    <img src="/header_logo.png" alt="" />
+                                </div>
+                                <div className={classes.footer_logoLine__left___title}>
+                                    Инвестиционный портал <br />
+                                    Карачаево-Черкесской Республики
+                                </div>
+                            </div>
+                            <div className={classes.footer_logoLine__right}>
+                                Инвестиционный портал Карачаево-Черкесской Республики ресурс,
+                                созданный специально для помощи предпринимателям и инвесторам.
+                                Здесь можно узнать, куда инвестировать в КЧР, а также о льготах
+                                для малого и среднего предпринимательства.
+                            </div>
+                        </div>
+
+                        <div className={classes.footer_infoLine}>
+                            <div className={classes.footer_infoLine__item}>
+                                <div className={classes.footer_infoLine__item___icon}>
+                                    <img src="/phone.png" alt="" />
+                                </div>
+                                <div className={classes.footer_infoLine__item___info}>
+                                    <div className={classes.footer_infoLine__item___info____title}>Телефон</div>
+                                    <div className={classes.footer_infoLine__item___info____desc}>8 (8782) 25-02-27</div>
+                                </div>
+                            </div>
+                            <div className={classes.footer_infoLine__item}>
+                                <div className={classes.footer_infoLine__item___icon}>
+                                    <img src="/mail.png" alt="" />
+                                </div>
+                                <div className={classes.footer_infoLine__item___info}>
+                                    <div className={classes.footer_infoLine__item___info____title}>Электронная почта</div>
+                                    <div className={classes.footer_infoLine__item___info____desc}>reception@moibiz09.ru</div>
+                                </div>
+                            </div>
+                            <div className={classes.footer_infoLine__item}>
+                                <div className={classes.footer_infoLine__item___icon}>
+                                    <img src="/place.png" alt="" />
+                                </div>
+                                <div className={classes.footer_infoLine__item___info}>
+                                    <div className={classes.footer_infoLine__item___info____title}>Наш адрес</div>
+                                    <div className={classes.footer_infoLine__item___info____desc}>Черкесск, проспект Ленина, 53</div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className={classes.footer_makeLine}>
+                            <div className={classes.footer_makeLine__left}>
+                                © Инвестиционный портал Карачаево-Черкесской Республики <br />
+                                При использовании материалов ссылка на сайт <span>инвестиционного портала</span> обязательна
+                            </div>
+                            <div className={classes.footer_makeLine__right}>
+                                <img src="/alazar_light.png" alt="" />
+                            </div>
+                        </div>
+                    </WidthBlock>
+                </CenterBlock>
+            </div>
         </>
-     );
+    );
 }
 
 export default Footer;
--- src/Components/Blocks/Main_page_block/Main_page_block.jsx
+++ src/Components/Blocks/Main_page_block/Main_page_block.jsx
@@ -14,6 +14,7 @@ import CenterBlock from '../../Standart/CenterBlock/CenterBlock'
 import ColumnBlock from '../../Standart/ColumnBlock/ColumnBlock'
 import H2 from '../../Standart/H2/H2'
 import Button from '../../Standart/Button/Button'
+import { Link } from "react-router-dom";
 
 function Main_page_block({ children, ...props }) {
     return (
@@ -171,17 +172,9 @@ function Main_page_block({ children, ...props }) {
                                 <img src="/map.png" alt="" />
                             </div>
                         </div>
-                    </WidthBlock>
-                </CenterBlock>
 
-                <CenterBlock>
-                    <WidthBlock>
                         <H2 text_align='center' text_transform="uppercase" color="var(--blue_color)">Ресурсы</H2>
-                    </WidthBlock>
-                </CenterBlock>
 
-                <CenterBlock>
-                    <WidthBlock>
                         <div className={classes.resourses}>
                             <a href="https://kchr.ru" target="_blank" className={classes.resourses_item}>
                                 <div className={classes.resourses_item__title}>Глава и Правительство</div>
@@ -200,14 +193,9 @@ function Main_page_block({ children, ...props }) {
                                 <div className={classes.resourses_item__linkName}>moibiz09.ru</div>
                             </a>
                         </div>
-                    </WidthBlock>
-                </CenterBlock>
 
-                <CenterBlock>
-                    <WidthBlock>
-                        <H2 text_align='center' text_transform="uppercase" color="var(--blue_color)">
-                            Реализованные и реализуемые проекты
-                        </H2>
+                        <H2 text_align='center' text_transform="uppercase" color="var(--blue_color)">Реализованные и реализуемые проекты</H2>
+
                         <CenterBlock>
                             <Button>Все проекты</Button>
                         </CenterBlock>
@@ -307,10 +295,146 @@ function Main_page_block({ children, ...props }) {
                                 </SwiperSlide>
                             </Swiper>
                         </div>
+
+                        <div className={classes.project_links}>
+                            <Link to={"#"} className={classes.project_links__item}>
+                                <div className={classes.project_links__item___img}>
+                                    <img src="/invest_1.png" alt="" />
+                                </div>
+                                <div className={classes.project_links__item___title}>
+                                    Инвестиционные предложения
+                                    <img src="/Arrow 1.png" alt="" />
+                                </div>
+                            </Link>
+                            <Link to={"#"} className={classes.project_links__item}>
+                                <div className={classes.project_links__item___img}>
+                                    <img src="/invest_2.png" alt="" />
+                                </div>
+                                <div className={classes.project_links__item___title}>
+                                    Приоритетные инвестиционные проекты
+                                    <img src="/Arrow 1.png" alt="" />
+                                </div>
+                            </Link>
+                            <Link to={"#"} className={classes.project_links__item}>
+                                <div className={classes.project_links__item___img}>
+                                    <img src="/invest_3.png" alt="" />
+                                </div>
+                                <div className={classes.project_links__item___title}>
+                                    База инвестиционных проектов
+                                    <img src="/Arrow 1.png" alt="" />
+                                </div>
+                            </Link>
+                        </div>
+
+                        <H2 text_align='center' text_transform="uppercase" color="var(--blue_color)">Новости</H2>
+
+                        <CenterBlock>
+                            <Button>Все новости</Button>
+                        </CenterBlock>
+
+                        <div className={classes.news}>
+                            <div className={classes.news_item}>
+                                <div className={classes.news_item__img}>
+                                    <img src="/new1.jpg" alt="" />
+                                </div>
+                                <div className={classes.news_item__desc}>
+                                    <div className={classes.news_item__desc___separate}>
+                                        <div className={classes.news_item__desc___date}>08.02.2022</div>
+                                        <div className={classes.news_item__desc___title}>
+                                            ПОДПИСАНИЕ ДОРОЖНОЙ КАРТЫ ПО РАЗВИТИЮ СОТРУДНИЧЕСТВА МЕЖДУ КАРАЧАЕВО-ЧЕРКЕССКОЙ
+                                            РЕСПУБЛИКОЙ И РЕСПУБ...
+                                        </div>
+                                        <div className={classes.news_item__desc___hash}>#Кредит #фот 3.0</div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className={classes.news_item}>
+                                <div className={classes.news_item__img}>
+                                    <img src="/new2.png" alt="" />
+                                </div>
+                                <div className={classes.news_item__desc}>
+                                    <div className={classes.news_item__desc___separate}>
+                                        <div className={classes.news_item__desc___date}>08.02.2022</div>
+                                        <div className={classes.news_item__desc___title}>
+                                            ЛЬГОТНЫЕ КРЕДИТЫ ПО ПРОГРАММЕ
+                                            ФОТ 3.0 ПОДДЕРЖАЛИ БОЛЕЕ 26 ТЫС.
+                                            КОМПАНИЙ И ПОМОГЛИ СОХРАНИТЬ
+                                            ОКОЛО 6...
+                                        </div>
+                                    </div>
+                                    <div className={classes.news_item__desc___hash}>#Кредит #фот 3.0</div>
+                                </div>
+                            </div>
+
+                            <div className={classes.news_item}>
+                                <div className={classes.news_item__img}>
+                                    <img src="/new3.jpg" alt="" />
+                                </div>
+                                <div className={classes.news_item__desc}>
+                                    <div className={classes.news_item__desc___separate}>
+                                        <div className={classes.news_item__desc___date}>08.02.2022</div>
+                                        <div className={classes.news_item__desc___title}>
+                                            ЗАСЕДАНИЕ СОВЕТА ПО ПРОЕКТНОИ
+                                            ДЕЯТЕЛЬНОСТИ КЧР...
+                                        </div>
+                                    </div>
+                                    <div className={classes.news_item__desc___hash}>#Кредит #фот 3.0</div>
+                                </div>
+                            </div>
+
+                            <div className={classes.news_item}>
+                                <div className={classes.news_item__img}>
+                                    <img src="/new1.jpg" alt="" />
+                                </div>
+                                <div className={classes.news_item__desc}>
+                                    <div className={classes.news_item__desc___separate}>
+                                        <div className={classes.news_item__desc___date}>08.02.2022</div>
+                                        <div className={classes.news_item__desc___title}>
+                                            ПОДПИСАНИЕ ДОРОЖНОЙ КАРТЫ ПО РАЗВИТИЮ СОТРУДНИЧЕСТВА МЕЖДУ КАРАЧАЕВО-ЧЕРКЕССКОЙ
+                                            РЕСПУБЛИКОЙ И РЕСПУБ...
+                                        </div>
+                                        <div className={classes.news_item__desc___hash}>#Кредит #фот 3.0</div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className={classes.news_item}>
+                                <div className={classes.news_item__img}>
+                                    <img src="/new2.png" alt="" />
+                                </div>
+                                <div className={classes.news_item__desc}>
+                                    <div className={classes.news_item__desc___separate}>
+                                        <div className={classes.news_item__desc___date}>08.02.2022</div>
+                                        <div className={classes.news_item__desc___title}>
+                                            ЛЬГОТНЫЕ КРЕДИТЫ ПО ПРОГРАММЕ
+                                            ФОТ 3.0 ПОДДЕРЖАЛИ БОЛЕЕ 26 ТЫС.
+                                            КОМПАНИЙ И ПОМОГЛИ СОХРАНИТЬ
+                                            ОКОЛО 6...
+                                        </div>
+                                    </div>
+                                    <div className={classes.news_item__desc___hash}>#Кредит #фот 3.0</div>
+                                </div>
+                            </div>
+
+                            <div className={classes.news_item}>
+                                <div className={classes.news_item__img}>
+                                    <img src="/new3.jpg" alt="" />
+                                </div>
+                                <div className={classes.news_item__desc}>
+                                    <div className={classes.news_item__desc___separate}>
+                                        <div className={classes.news_item__desc___date}>08.02.2022</div>
+                                        <div className={classes.news_item__desc___title}>
+                                            ЗАСЕДАНИЕ СОВЕТА ПО ПРОЕКТНОИ
+                                            ДЕЯТЕЛЬНОСТИ КЧР...
+                                        </div>
+                                    </div>
+                                    <div className={classes.news_item__desc___hash}>#Кредит #фот 3.0</div>
+                                </div>
+                            </div>
+                        </div>
                     </WidthBlock>
                 </CenterBlock>
-
-
             </ColumnBlock>
         </>
     );
