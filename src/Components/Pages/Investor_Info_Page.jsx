459f9c8d10684ec919b947e2eb6840b5f71b2f70
alimdzhatdoev <alimdzhatdoev1@gmail.com>
2024-05-23 10:41:04 +0300

    Update

--- src/Components/Pages/Investor_Info_Page.jsx
+++ src/Components/Pages/Investor_Info_Page.jsx
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 import Invest_pojects_block from "../Blocks/Invest_pojects_block/Invest_pojects_block";
 import RIS_project_block from "../Blocks/RIS_project_block/RIS_project_block";
 import Mech_project_block from "../Blocks/Mech_project_block/Mech_project_block";
+import Gis_kapitel_block from "../Blocks/Gis_kapitel_block/Gis_kapitel_block";
 
 function Investor_Info_Page({ children, ...props }) {
     let { id } = useParams();
@@ -14,7 +15,7 @@ function Investor_Info_Page({ children, ...props }) {
             {id == 'investicionnye_proekty' ? <Invest_pojects_block /> : null}
             {id == 'regionalnyj_investicionnyj_standart' ? <RIS_project_block /> : null}
             {id == 'dejstvuyushchie_mekhanizmy_gosudarstvennogo_stimulirovaniya_investicionnoj_deyatelnosti' ? <Mech_project_block /> : null}
-            {id == 'gis_kapitalovlozheniya' ? <Mech_project_block /> : null}
+            {id == 'gis_kapitalovlozheniya' ? <Gis_kapitel_block /> : null}
             {/* {id == 'kadrovoe_obespechenie' ? <With_title /> : null} */}
             {/* {id == 'dokuments' ? <With_title /> : null} */}
         </>
